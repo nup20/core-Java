24b8968bdb557d6c565d3d9a347bf83b78f793c6
nup20 <130826435+nup20@users.noreply.github.com>
2023-06-26 06:12:01 -0700

    Add files via upload

---
+++ Array/Sum.java
@@ -0,0 +1,29 @@
+ public class Sum
+{
+public static void main(String s[])
+{
+int a,b,c,d,num;
+System.out.println("Input an integer between 0 and 1000:");
+num=Integer.parseInt(sc.nextLine());
+if((num>0)&&(num<1000))
+{
+
+
+System.out.println("The sum of all digits in " +a+ " is="+a);
+}
+
+
+
+}
+
+}
+
+
+
+
+
+/*Test Data
+Input an integer between 0 and 1000: 565
+Expected Output :
+The sum of all digits in 565 is 16
+*/
