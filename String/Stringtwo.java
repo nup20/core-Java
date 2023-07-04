7ec2bc043bfbd99329f20678a01bd9adde84f198
nup20 <130826435+nup20@users.noreply.github.com>
2023-07-04 04:46:26 -0700

    Add files via upload

---
+++ String/Stringtwo.java
@@ -0,0 +1,26 @@
+package Stringpro;
+
+public class Stringtwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "This is Exercise 1";
+		String  b= "This is Exercise 2";
+		            System.out.println(a+" \n"+b);
+		            int c=a.compareTo(b);
+		            if (c==0)
+		            		System.out.println("This is Exercise 1 is Equal to This is Exercise 2");
+		            		if(c>0)
+		            			System.out.println("This is Exercise 1 is greater  than This is Exercise 2");
+		            			if(c<0)
+		            				System.out.println("This is Exercise 1 is less than This is Exercise 2");
+		            		
+
+	}
+
+}
+/*Posted Apr 30, 2020
+Assigned
+String 1: This is Exercise 1
+String 2: This is Exercise 2
+"This is Exercise 1" is less than "This is Exercise 2"*/
