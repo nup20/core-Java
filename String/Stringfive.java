c358773197a52c932f254a858059f3ae780507e1
nup20 <130826435+nup20@users.noreply.github.com>
2023-07-06 05:46:18 -0700

    Add files via upload

---
+++ String/Stringfive.java
@@ -0,0 +1,15 @@
+package Stringpro;
+
+public class Stringfive {
+
+	public static void main(String[] args) {
+		String a = "PHP Exercises and ";
+		String  b= "Python Exercises";
+		            System.out.println("The concatenated string:"+a +b);
+
+	}
+
+}
+
+
+ 
