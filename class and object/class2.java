2210ad3c650546430a7a46a9e9e859face8a315b
nup20 <130826435+nup20@users.noreply.github.com>
2023-07-14 21:41:45 -0700

    Add files via upload

---
+++ class and object/class2.java
@@ -0,0 +1,18 @@
+package Class;
+
+class Student
+{
+	String name;
+	int roll_no;
+	
+}
+public class class2 {
+
+	public static void main(String[] args) {
+		Student s=new Student();
+		s.name="john";
+		s.roll_no=2;
+		System.out.print("  name="+s.name+"\n roll_no"+s.roll_no);
+	}
+
+}
