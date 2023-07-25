b4b510c1ca70767b94591e45e6131cfb9f0dc146
nup20 <130826435+nup20@users.noreply.github.com>
2023-07-24 19:36:54 -0700

    Add files via upload

---
+++ String/Stringsix.java
@@ -0,0 +1,14 @@
+
+
+public class Stringsix {
+
+	public static void main(String[] args) {
+		String a = "PHP Exercises and Python Exercises";
+ System.out.println("Original String:"+a);
+ 
+ System.out.println("Specified sequence of char values:");
+ String b=new String ();
+ a.contains(b);
+	}
+
+}
