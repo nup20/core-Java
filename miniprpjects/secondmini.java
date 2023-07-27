f3b2a17f6fe3fe1e9bb5f8cc26f6f2c0afaa5dd0
nup20 <130826435+nup20@users.noreply.github.com>
2023-07-26 20:41:37 -0700

    Add files via upload

---
+++ miniprpjects/secondmini.java
@@ -0,0 +1,40 @@
+package miniproject2;
+
+import java.util.Scanner;
+
+class  Student{
+	int roll,A,B,C;
+	void fun()
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		for(int i=0; i<=8; i++)
+		{
+			for( i=0; i<=8; i++)
+			{
+			System.out.print("Enter Roll number :");
+			roll =sc.nextInt();
+			System.out.print("Enter First subject Marks :");
+			A = sc.nextInt();
+			System.out.print("Enter second subject marks:");
+			B= sc.nextInt();
+			System.out.print("Enter third subject marks :");
+			C = sc.nextInt();
+			int avg = (A +B + C)/3 ;
+			System.out.println("Student Roll No "+roll); 
+			System.out.println("Student Avarage mark "+avg);
+			System.out.println("\n ");
+
+	}
+	}
+}
+}
+public class secondmini {
+
+	public static void main(String[] args) {
+		Student s=new Student();
+        s.fun();
+
+	}
+
+}
