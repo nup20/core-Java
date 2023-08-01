576fd076b4d86d5d91d10820f2f251d6c1175738
nup20 <130826435+nup20@users.noreply.github.com>
2023-07-31 19:58:59 -0700

    Update class3.java

--- miniprpjects/class3.java
+++ miniprpjects/class3.java
@@ -4,11 +4,21 @@ class BankAccount{
 	String name;
 	String Addressofdepositor;
 	String Typeaccount;
-	
 	Double Balanceacc;
 	Double Numbertransactions;
-	
+	Double D,W,T;
+	int i=100;
 	Scanner sc= new Scanner(System.in);
+	void UniqueAccno()
+	{int un;
+		//while(i)
+		{
+			System.out.println("Enter the unique no=BA"+i);
+			
+		}
+		i++;
+			
+	}
 	
 	void EDetails()
 	{System.out.println("Enter the Name=");
@@ -19,9 +29,37 @@ class BankAccount{
 	Balanceacc=Double.parseDouble(sc.nextLine());
 		
 	}
+	void Deposite()	
+	{ 
+	   this. Balanceacc=Balanceacc;
+	   System.out.println("Enter the Deposite=");
+	   D=Double.parseDouble(sc.nextLine());
+	   Balanceacc+=D;
+	}
+	void Withsdrawl()
+	{
+	System.out.println("Enter the  Withsdrawl=");
+	W=Double.parseDouble(sc.nextLine());
+	this. Balanceacc=Balanceacc;
+	Balanceacc-=W;
+	}
+	void Transfer()
+	{
+		System.out.println("Enter the  Transfer=");
+		T=Double.parseDouble(sc.nextLine());
+		this. Balanceacc=Balanceacc;
+		Balanceacc-=T;
+	}
 	
-	void Withsdrawl
-	
+	void AllDetails()
+	{
+		System.out.println("Enter the  Name="+name);
+		System.out.println("Enter the  Uniqueno="+i);
+		System.out.println("Enter the  Withdrawl="+W);
+		System.out.println("Enter the  deposite="+D);
+		System.out.println("Enter the  Transfer="+T);
+		System.out.println("Enter the  Balance Ammount="+Balanceacc);
+	}
 }
 public class class3 {
 
@@ -29,9 +67,11 @@ public class class3 {
 	BankAccount B=new BankAccount();
 		System.out.println("++++++++++++Welcome to  Bank++++++++++++");
 		B.EDetails();
-		
-		
-		
+		B.UniqueAccno();
+		B.Deposite();
+		B.Withsdrawl();
+		B.Transfer();
+		B.AllDetails();
 	}
 
 }
