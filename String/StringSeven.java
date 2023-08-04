b33e4805949296c11cee4618ea0599707ec86a95
nup20 <130826435+nup20@users.noreply.github.com>
2023-08-03 19:52:16 -0700

    Update and rename Array.java to StringSeven.java

---
+++ String/StringSeven.java
@@ -0,0 +1,15 @@
+package Stringpro;
+
+public class Stringseven {
+
+	public static void main(String[] args) {
+		String s1="Comparing Example.com";
+		String s2="Comparing example.com";
+		System.out.println("Comparing Example.com and Comparing example.com ="+s1.contentEquals(s2));
+		System.out.println("Comparing Example.com and Comparing Example.com="+s1.contentEquals(s1));
+		
+		
+	
+
+}
+}
