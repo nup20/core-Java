182515bd4f3f5db388ee9c99f87cbd2b0dc0a575
nup20 <130826435+nup20@users.noreply.github.com>
2023-09-19 21:52:27 -0700

    Update test2.java

--- this/test2.java
+++ this/test2.java
@@ -1,12 +1,14 @@
 package program1;
 class All{
 	int roll;
-	String name;
+	String name,course;
 	double phno;
-	All(int r,String s,double p)
+	
+	All(int r,String s,double p,String c)
 	{roll=r;
 	name=s;
 	phno=p;
+	 course=c;
 		
 	}
 	void fun()
@@ -19,7 +21,7 @@ public class test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         All f=new All(9,"neha",67890);
-        All n=new All(90,"nehhhha",69065);
+        All n=new All(90,"nehhhha",69065,"cpp");
         f.fun();
         n.fun();
         
