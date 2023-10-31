939b45e4892ffc5ebb0b8d32bb77c2083f909500
nup20 <130826435+nup20@users.noreply.github.com>
2023-10-31 05:53:50 -0700

    Add files via upload

---
+++ constructor/defaultt.java
@@ -0,0 +1,17 @@
+package pack1;
+class Student{
+  int id;
+  String name;
+	void  A() {
+		System.out.println(id+" "+name);
+	}
+}
+public class defaultt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Student s=new Student();
+       s.A();
+	}
+
+}
