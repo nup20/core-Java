35ae64b3724e2b9be019e98133b808346b78a408
nup20 <130826435+nup20@users.noreply.github.com>
2023-11-06 05:12:56 -0800

    Add files via upload

---
+++ constructor/const1.java
@@ -0,0 +1,16 @@
+package pack1;
+
+class Bike1{  
+	//creating a default constructor  
+	Bike1(){System.out.println("Bike is created");}  
+	
+	
+	}  
+public class const1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bike1 b=new Bike1();
+	}
+
+}
