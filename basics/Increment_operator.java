1f8f0aa3fc76e0497f1668851e94b1834603ff31
nup20 <nupurthakre16@gmail.com>
2023-11-14 13:47:04 +0530

    add

---
+++ basics/Increment_operator.java
@@ -0,0 +1,18 @@
+package operators;
+
+public class Increment_operator {
+
+	public static void main(String[] args) {
+		
+int a = 10, b = 20 ,c;
+		
+		c = ( ++a ) + ( ++b );
+		
+		
+		
+		System.out.println( " Value of a = " + a );
+		System.out.println( " Value of b = " + b );
+		System.out.println( " Value of c = " + c );
+	}
+
+}
