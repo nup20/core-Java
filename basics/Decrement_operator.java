d93df77be322e81bbf709020b76120aaa363d313
nup20 <nupurthakre16@gmail.com>
2023-11-21 09:47:07 +0530

    add

---
+++ basics/Decrement_operator.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class Decrement_operator {
+
+	public static void main(String[] args) {
+         
+		int a = 10, b = 20 ,c;
+		
+		c = ( --a ) + ( --b );
+		
+		System.out.println( " Value of a = " + a );
+		System.out.println( " Value of b = " + b );
+		System.out.println( " Value of c = " + c );
+
+	}
+
+}
