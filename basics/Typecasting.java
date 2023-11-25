9d2f936cf750fcfa992001bca4311a9186919505
nup20 <nupurthakre16@gmail.com>
2023-11-25 21:25:25 +0530

    created

---
+++ basics/Typecasting.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		int a;
+		float b = 123.333f;
+		System.out.println( " Before Casting = " + b );
+
+		a = (int) b;
+		
+		System.out.println( " After Casting  = " + a );
+	}
+
+}
