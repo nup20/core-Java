f99eb9fa0ce28e46fdc32488763886fc7657794e
nup20 <nupurthakre16@gmail.com>
2023-11-26 23:50:09 +0530

    add conditionalor

---
+++ basics/conditionalopr.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class conditionalopr {
+	
+		public static void main( String args[ ] )
+		{
+	       	int a, b, max;
+
+			a = 25;
+			b = 14;
+
+			max = ( ( a > b ) ? a : b );
+			System.out.print("\nMaximum Value is= " + max );
+		}
+	}
