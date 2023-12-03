3ad1bb72fd5f7f54688a8702afd797baa95e67e3
nup20 <nupurthakre16@gmail.com>
2023-12-03 22:05:01 +0530

    add

---
+++ basics/ifelse.java
@@ -0,0 +1,21 @@
+package ifelse;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+int  n;
+		
+		n = 23;
+
+		if( n > 100 )
+		{
+			System.out.println( " The number is greater than 100." );
+		}	
+		else
+		{
+			System.out.println( " The number is smaller than 100." );
+		}
+
+	}
+
+}
