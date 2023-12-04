30838dce877fd2f6bcde815167406637d23f053b
nup20 <nupurthakre16@gmail.com>
2023-12-04 22:17:55 +0530

    add

---
+++ basics/Evenodd.java
@@ -0,0 +1,17 @@
+package ifelse;
+
+public class Evenodd {
+
+	public static void main(String[] args) {
+		int n;
+
+		n = 23;
+
+		if( n % 2 == 0 )
+			System.out.println("Number " + n + " is Even Number." );
+		else
+			System.out.println("Number " + n + " is Odd Number." );
+
+	}
+
+}
