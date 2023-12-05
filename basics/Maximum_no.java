8bd16e70f394c63e77c456b96018656aaa615441
nup20 <nupurthakre16@gmail.com>
2023-12-05 22:26:26 +0530

    add

---
+++ basics/Maximum_no.java
@@ -0,0 +1,20 @@
+package ifelse;
+
+public class Maximum_no {
+
+	public static void main(String[] args) {
+		int a, b, max;
+
+		a = 23;
+		b = 14;
+
+		if( a > b )
+			max = a;
+		else
+			max = b;
+	    
+		System.out.println("\nMaximum : " + max );
+	
+	}
+
+}
