a632f1819f1a6c1da3e1bc5cd24551849a65f7c5
nup20 <nupurthakre16@gmail.com>
2023-12-06 22:31:04 +0530

    add

---
+++ basics/nestedif.java
@@ -0,0 +1,27 @@
+package ifelse;
+
+public class nestedif {
+
+	public static void main(String[] args) {
+		int a = 10;
+		   
+		   if(a>0){
+		       if(a<=10){
+		           System.out.println("a --> 0 to 10");
+		       }
+		       
+		       if(a>10 && a<=20){
+		           System.out.println("a --> 11 to 20");
+		       }
+		       
+		       if(a>20){
+		           System.out.println("a --> 21 to ((2^31 -1)= 2,14,74,83,647");
+		       
+		       
+		   
+
+	}}
+	}
+}
+
+
