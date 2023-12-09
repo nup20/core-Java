5aeab10bbbf93378f2d40c49fb92131b0334260f
nup20 <nupurthakre16@gmail.com>
2023-12-09 22:50:08 +0530

    add

---
+++ basics/ifelsedemo.java
@@ -0,0 +1,26 @@
+package ifelse;
+
+public class ifelsedemo {
+
+	public static void main(String[] args) {
+		int a = 2;
+
+		if (a == 1) 
+		{
+			System.out.println("One");
+		} 
+		else if (a == 2) 
+		{
+			System.out.println("Two");
+		} 	
+		else if (a == 3) 
+		{
+			System.out.println("Three");
+		} 
+		else 
+		{
+			System.out.println("invalid Value.");
+		}
+	}
+
+}
