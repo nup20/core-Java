49c8a7e7a1d006408897f962b755672e7d42157f
nup20 <nupurthakre16@gmail.com>
2023-12-24 09:21:12 +0530

    Add primeno.java file

---
+++ basics/primeno.java
@@ -0,0 +1,20 @@
+package operators;
+
+
+import java.util.Scanner;
+
+public class primeno {
+
+	public static void main(String arg[]){
+		
+		
+		Scanner sc=new Scanner(System.in);
+		int a =sc.nextInt();
+		System.out.println("Enter  no=");
+		if((a%a==a)||(a%1==a)){
+			System.out.println("prime no="+a);
+			
+		}
+		
+	}
+}
