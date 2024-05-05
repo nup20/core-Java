caf76485b47587e202f53c53d7b7fcd1edd0ae96
nup20 <nupurthakre16@gmail.com>
2024-05-06 00:16:09 +0530

    created

---
+++ basics/modulus.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class modulus {
+
+	public static void main(String[] args) {
+		int x = 42;
+		double y = 42.25;
+		float h=665;
+		System.out.println("x mod 10 = " + x % 10);
+		System.out.println("y mod 10 = " + y % 10);
+		System.out.println("y mod 10 = " + h % 10);
+	}
+
+}
