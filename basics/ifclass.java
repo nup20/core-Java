76ba65e617b98b675e391f928149143eaf521169
nup20 <nupurthakre16@gmail.com>
2024-06-06 22:03:21 +0530

    add

---
+++ basics/ifclass.java
@@ -0,0 +1,23 @@
+package ifelse;
+
+public class ifclass {
+
+	public static void main(String[] args) {
+		  int a = 10;
+	        int b = 20;
+	        
+	        if(a>b){
+	            System.out.println("a is greater than b");
+	        }
+	        
+	        if(a<b){
+	            System.out.println("a is less than b");
+	        }
+	        
+	        if(a==b){
+	            System.out.println("a is equal to b");
+	        }
+
+	}
+
+}
